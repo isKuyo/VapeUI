0ace8e50c7316492fe59e1111b6f635c23f3b280
hxnq <76233782+isKuyo@users.noreply.github.com>
2026-01-06 21:58:05 -0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1,3 +1,4 @@
+print("a")
 local mainapi = {
 	Categories = {},
 	GUIColor = {
@@ -314,7 +315,7 @@ local function downloadFile(path, func)
 	if not isfile(path) then
 		createDownloader(path)
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/isKuyo/VapeUI/main/'..select(1, path:gsub('newvape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
